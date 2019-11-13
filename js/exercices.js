89280066af4a2bd299824277e21dcbea0c591909
Bilal BOUALEM <bilal.boualem1@gmail.com>
2019-11-13 01:37:38 +0100

    corriger chaise en chaise et table

--- js/exercices.js
+++ js/exercices.js
@@ -3,7 +3,7 @@ var exercices = {
         "Modelisation_Chaise_visiteurs",
         "Modelisation_porte_Capitonne",
         "Modelisation_Table",
-        "Modelisation_Chaise",
+        "Modelisation_Chaise_et_table",
         "Modelisation_Bureau_L"
     ]
 }
