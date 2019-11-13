9649960465e0446537ed26cc40c22edd55c1643b
Bilal BOUALEM <34511873+boualemb@users.noreply.github.com>
2019-11-13 21:56:36 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ function init() {
     var canvs= document.getElementByTagName('canvas');
     scene = new THREE.Scene();
     renderer = new THREE.WebGLRenderer( { antialias: true } );
-    controls = new THREE.OrbitControls( camera, canvs );
+    controls = new THREE.OrbitControls( camera, renderer.domElement );
     const ambientLight = new THREE.AmbientLight( 0x404040 );
     mainLight = new THREE.DirectionalLight( 0xc4c4c4,1 );
     mainLight.position.set( 10, 10, 10 );
